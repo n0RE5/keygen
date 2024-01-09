3f9bc77b7997dbeadd8a49bc7565fdcf553be304
Your Name <you@example.com>
2024-01-09 19:44:06 +0300

    updated router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,10 +1,17 @@
+import { keyLoader } from "@/api/keysAPI"
 import KeygenPage from "@/pages/KeygenPage"
+import ValidatePage from "@/pages/ValidatePage"
 import { RouteObject, createBrowserRouter } from "react-router-dom"
 
 const routes: RouteObject[] = [
     {
         path: '/',
         element: <KeygenPage/>,
+        loader: keyLoader
+    },
+    {
+        path: '/validate',
+        element: <ValidatePage />
     }
 ]
 
