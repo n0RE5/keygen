1c70f1d57df48b1a5ce611bfbfccb3a691ac5ddc
Your Name <you@example.com>
2024-01-09 19:56:44 +0300

    added key controller

--- server/src/keys/keys.controller.ts
+++ server/src/keys/keys.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { KeysService } from './keys.service';
+import { IpAddress } from 'src/decorators/ipaddress';
 
 @Controller('keys')
-export class KeysController {}
+export class KeysController {
+
+    constructor(private keyService: KeysService) {}
+
+    @Get()
+    generate(@IpAddress() userIp) {
+        return this.keyService.generate(userIp)
+    }
+
+    @Post()
+    validate(@Body('uuid') uuid: string) {
+        return this.keyService.validate(uuid)
+    }
+}
