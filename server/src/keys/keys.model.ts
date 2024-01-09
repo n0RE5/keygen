0d76639cb429a2f43faf3b5f440da54453f3cae2
Your Name <you@example.com>
2024-01-09 19:56:54 +0300

    added key model

--- server/src/keys/keys.model.ts
+++ server/src/keys/keys.model.ts
@@ -3,16 +3,20 @@ import { Column, DataType, Model, Table } from "sequelize-typescript";
 interface KeyCreationAttrs {
     uuid: string;
     expiresIn: number;
+    timestamp: number;
     userIp: string;
 }
 
-@Table({tableName: 'keys'})
+@Table({tableName: 'keys', createdAt: false, updatedAt: false})
 export class Key extends Model<Key, KeyCreationAttrs> {
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     uuid: string;
+
+    @Column({type: DataType.BIGINT})
+    timestamp: number;
 
     @Column({type: DataType.INTEGER})
     expiresIn: number;
