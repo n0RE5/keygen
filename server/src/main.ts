b0c9b051272ec06e83007843269074de088e01e7
Your Name <you@example.com>
2024-01-09 19:56:19 +0300

    added request-ip middleware

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { AppModule } from "./app.module"
 import { NestFactory } from "@nestjs/core"
+import * as requestIp from 'request-ip'
 
 const start = async () => {
     const PORT = process.env.PORT || 5000
@@ -8,6 +9,7 @@ const start = async () => {
         bodyParser: true
     })
     app.enableCors();
+    app.use(requestIp.mw())
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
 
