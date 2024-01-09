a05c2978f2b16509d26d3fc1cef3bae09a40a3b6
Your Name <you@example.com>
2024-01-09 19:57:00 +0300

    added key service

--- server/src/keys/keys.service.ts
+++ server/src/keys/keys.service.ts
@@ -1,19 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Key } from './keys.model';
 import { KEY_EXPIRES_IN } from 'src/utils/consts';
-import * as uuid from 'uuid'
+import * as uuidGen from 'uuid'
 
 @Injectable()
 export class KeysService {
 
     constructor(@InjectModel(Key) private keyRepository: typeof Key) {}
 
-    async generate() {
-        const keyId = uuid.v4()
-        // Time in seconds
+    async generate(userIp: string) {
+        const uuid = uuidGen.v4()
+        const timestamp = Date.now()
         const expiresIn = KEY_EXPIRES_IN
-        const key = await this.keyRepository.create({uuid: keyId, expiresIn})
+        const avaiableKey = await this.findAvailableKey(userIp)
+        if(avaiableKey) {
+            return avaiableKey.uuid
+        }
+        const key = await this.keyRepository.create({
+            uuid, 
+            timestamp,
+            expiresIn,
+            userIp
+        })
         return key.uuid
+    }
+
+    async findAvailableKey(userIp: string) {
+        const keys = await this.keyRepository.findAll({where: {userIp}})
+        const timestamp = Date.now()
+        for (let key of keys) {
+            const expired = (timestamp - key.createdAt) > key.expiresIn
+            if(!expired) {
+                return key
+            }
+        }
+        return false
+    }
+
+    async validate(uuid: string) {
+        const timestamp = Date.now()
+        const key = await this.keyRepository.findOne({where: {uuid}})
+        if(!key) {
+            throw new HttpException("Key doesn't exists.", HttpStatus.BAD_REQUEST)
+        }
+        const expired = (timestamp - key.createdAt) > key.expiresIn
+        console.log(key.createdAt)
+        if(expired) {
+            await this.keyRepository.destroy({where: {uuid}})
+            throw new HttpException("Expired key.", HttpStatus.FORBIDDEN)
+        }
+        return true
     }
 }
